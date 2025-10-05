bd81632ee1ae47748e388fa53b1202564852edc4
Mohammed Dhariq I B <ibmohammeddhariq@email.com>
2025-10-05 15:55:23 -0700

    Change the base to swappify2.0

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", // Use relative paths for GitHub Pages
+  base: "/Swappify-2.0/", // Use relative paths for GitHub Pages
   server: {
     host: "::",
     port: 8080,
